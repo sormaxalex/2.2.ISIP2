86d10e624e85d0740287bd6c30a833a75fc45742
sormaxalex <128036375+sormaxalex@users.noreply.github.com>
2023-03-18 09:58:25 +0300

    УП.1.1.в1

---
+++ WindowsFormsApp21/WindowsFormsApp21/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string[] mas = textBox1.Lines;
+            string[] mas1 = mas.Distinct().ToArray();
+            List <int> mas_sch = new List<int>();
+            foreach (string s1 in mas1)
+            {
+                int sch = 0;
+                foreach (string s2 in mas)
+                {
+                    if (s1 == s2)
+                    {
+                        sch++;
+                    }
+                }
+                mas_sch.Add(sch);                
+            }
+            for (int i = 0; i < mas_sch.Count; i++)
+                {
+                if (mas_sch[i] == mas_sch.Max())
+                    {
+                    listBox1.Items.Add(mas1[i]);
+                    }
+                }
+        }
+    }
+}
